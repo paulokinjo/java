2b5fb18262b968344089a8e529e4abea2b41a576
Paulo Kinjo <paulokinjo@hotmail.com>
2021-12-18 02:12:52 +0000

    Add sample jave gradle project

---
+++ javestboot/src/main/java/com/kinsoftwares/javestboot/JavestbootApplication.java
@@ -0,0 +1,13 @@
+package com.kinsoftwares.javestboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavestbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavestbootApplication.class, args);
+	}
+
+}
